a9764c43ee8ba05973c16842b74526abe1a497c6
Andrea Mainella <andrea.mainella98@gmail.com>
2022-02-28 16:16:30 +0100

    [FIX]    - Formatted code

--- lib/src/phone_state.dart
+++ lib/src/phone_state.dart
@@ -5,13 +5,13 @@ import 'package:phone_state/src/utils/phone_state_status.dart';
 
 /// PhoneState is a helper class that is used to be able to call the stream of PhoneState
 class PhoneState {
-  static const EventChannel _eventChannel = EventChannel(Constants.EVENT_CHANNEL);
+  static const EventChannel _eventChannel =
+      EventChannel(Constants.EVENT_CHANNEL);
 
   /// This method allows you to have a stream of the system phone state change
   static Stream<PhoneStateStatus?> get phoneStateStream {
-    return _eventChannel
-        .receiveBroadcastStream()
-        .distinct()
-        .map((dynamic event) => EnumToString.fromString<PhoneStateStatus>(PhoneStateStatus.values, event));
+    return _eventChannel.receiveBroadcastStream().distinct().map(
+        (dynamic event) => EnumToString.fromString<PhoneStateStatus>(
+            PhoneStateStatus.values, event));
   }
 }
